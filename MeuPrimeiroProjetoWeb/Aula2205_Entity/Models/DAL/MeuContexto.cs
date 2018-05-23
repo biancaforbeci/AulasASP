b79a61618f7d4c4a169df73f86065655a9c67c31
Bianca Forbeci <biaformic@hotmail.com>
2018-05-22 21:58:50 -0300

    Implementando BD

---
+++ MeuPrimeiroProjetoWeb/Aula2205_Entity/Models/DAL/MeuContexto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Aula2205_Entity.Models.DAL
+{
+    public class MeuContexto :DbContext
+    {
+        public MeuContexto(): base("strConn")
+        {
+            //  DropCreateDatabaseAlways
+            // DropCreateDatabaseIfModelChanges Se houver mudança na modelo ele apaga e cria de novo.
+            // Migrations (para por sistema em produção)
+            Database.SetInitializer<DropCreateDatabaseIfModelChanges>
+        }
+
+        
+
+
+
+    }
+}
